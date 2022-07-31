f989c0dbcea5e02038101594f2b14f2165c6800a
Jesus Gutierrez <jesus.gutierrez@ilumps.com>
2022-07-31 12:35:29 -0600

    Generates nodejs project, docker setup

---
+++ notificationsapi/src/models/index.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require('sequelize');
+
+// Option 1: Passing a connection URI
+const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
+
+module.exports = {
+  sequelize
+}
